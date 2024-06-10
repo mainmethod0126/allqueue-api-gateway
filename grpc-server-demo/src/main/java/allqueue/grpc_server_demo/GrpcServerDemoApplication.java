6e53243a9481b436511047ce619c5fb75d89e5b3
wusub.shin@softcamp.co.kr <wusub.shin@softcamp.co.kr>
2024-06-10 20:54:15 +0900

    UDPATE grpc 서버 추가 및 eureka 동작 확인

---
+++ grpc-server-demo/src/main/java/allqueue/grpc_server_demo/GrpcServerDemoApplication.java
@@ -0,0 +1,13 @@
+package allqueue.grpc_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcServerDemoApplication.class, args);
+	}
+
+}
---
+++ grpc-server-demo/src/test/java/allqueue/grpc_server_demo/GrpcServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package allqueue.grpc_server_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class GrpcServerDemoApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
